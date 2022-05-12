d5324538b025beb4689a3bd8c25f0a9a44b25de0
Andreas Taylor <Andy4495@users.noreply.github.com>
2022-05-12 12:41:01 -0500

    Update testscript.sh

--- tools/testscript.sh
+++ tools/testscript.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-python3 ./markdown_table_codegen.py <./tools/testdata/testfile-input.txt >./testfile-output.txt
+chmod +x ./tools/markdown_table_codegen.py
+
+python3 ./tools/markdown_table_codegen.py <./tools/testdata/testfile-input.txt >./testfile-output.txt
 
 diff ./testfile-output.txt ./tools/testdata/output-check.txt
 
