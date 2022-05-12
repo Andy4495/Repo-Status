45a097397b271374cc393d4d2112457e5cb82043
Andreas Taylor <Andy4495@outlook.com>
2022-05-12 12:51:16 -0500

    Updates

--- tools/testscript.sh
+++ tools/testscript.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# https://github.com/Andy4495/Repo-Status
+
+# Script to verify markdown_table_codegen.py 
+# Run the script from the top level of repo
+
 chmod +x ./tools/markdown_table_codegen.py
 
 python3 ./tools/markdown_table_codegen.py <./tools/testdata/testfile-input.txt >./testfile-output.txt
