1b3b1ad0e94ba1ce5d98f4cc96cab329dace3234
Andreas Taylor <Andy4495@outlook.com>
2022-05-15 18:05:36 -0500

    Add script

---
+++ tools/action-checkin.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# https://github.com/Andy4495/Repo-Status
+
+# Script to checkin file inside GitHub action if it changed
+# Required first parameter is original file
+# Required second parameter is updated file, and is the file that will be pushed to repo
+
+diff $1 $2
+
+result=$?
+
+if [ result -ne 0]
+then
+  git config --global user.email "Andy4495@users.noreply.github.com"
+  git config --global user.name "Andreas Taylor"
+  git add $2
+  git commit -m "Update $2 by generate-readme.yml action"
+  git push origin master
+fi
+
+
+
+
